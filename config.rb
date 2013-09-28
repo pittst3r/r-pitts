df7c2152a80b49110ee7bc4710de5667c5d1f206
Robbie Pitts <robbie@sweatypitts.com>
2013-09-28 12:00:06 -0400

    Update deploy setup

--- config.rb
+++ config.rb
@@ -45,6 +45,11 @@
 #   end
 # end
 
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+end
+
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
@@ -68,11 +73,6 @@ configure :build do
   
   set :haml, { :format => :html5 }
   set :sass
-  
-  activate :deploy do |deploy|
-    deploy.build_before = true
-    deploy.method = :git
-  end
   
 end
 
